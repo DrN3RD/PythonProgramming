b4c89850920107ddf2d427b148f08d164eb307b6
DrN3RD <hugo.danielson@hotmail.com>
2019-01-15 09:37:11 +0100

    Learning how to manipulate classes

---
+++ Chapter 10/button.py
@@ -0,0 +1,51 @@
+
+
+from graphics import *
+
+
+class Button:
+
+
+    def __init__(self,win,center,width,height,label):
+
+        qb = Button(myWin, centerPoint,width,height,'Quit')
+        w,h = width/2.0 , height/2.0
+        x,y = center.getX(),center.getY()
+
+        self.xmax, self.xmin = x+w,x-w
+        self.ymax, self.ymin = y+w, y-w
+
+        p1 = Point(self.xmin,self.ymin)
+        p2 = Point(self.xmax,self.ymax)
+
+        self.rect = Rectangle(p1,p2)
+        self.rect.setFill('lightgray')
+        self.rect.draw(win)
+        self.label = Text(center,label)
+        self.label.draw(win)
+        self.deactivate()
+
+    def activate(self):
+
+        """Sets this button to activate"""
+
+        self.label.setFill('black')
+        self.rect.setWidth(2)
+        self.active = True
+
+
+    def deactivate(self):
+        """Sets this button to inactive"""
+
+        self.label.setFill('darkgrey')
+        self.rect.setWidth(1)
+        self.active = False
+
+    def clicked(self,p):
+
+        return (self.active and
+                self.xmin <= p.getX()<=self.xmax and
+                self.ymin <= p.getY()<self.ymax)
+
+    def getLabel(self):
+        return self.label.getText()
